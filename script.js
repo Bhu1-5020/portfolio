68e8871b13618d84ecd5b226f2831bbc38091677
Bhu1-5020 <bhuwan.thapa88@yahoo.com>
2026-07-02 10:00:02 +0200

    Profile update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 async function loadHTML(elementID , file) {
     try{
 
@@ -9,6 +10,30 @@ async function loadHTML(elementID , file) {
         console.error(error);
     }
 }
-loadHTML("navbar" , "navbar.html");
-loadHTML("content" , "content.html");
-loadHTML("footer" , "footer.html");
+
+// typing effect
+
+const text = " Hi, I am Bhuwan a Junior Full-Stack Developer" ; 
+let i = 0 ; 
+function type(){
+    const element =  document.getElementById("type");
+  if(i < text.length){
+    element.innerHTML += text.charAt(i);
+    i++ ;
+    setTimeout(type , 300);
+  }
+}
+setInterval(() =>{
+    document.getElementById("cursor").classList.toggle("invisible");
+}, 550);
+
+async function init() {
+    await loadHTML("navbar", "navbar.html");
+    await loadHTML("content", "content.html");
+    await loadHTML("footer", "footer.html");
+
+    type();
+
+}
+
+init();
